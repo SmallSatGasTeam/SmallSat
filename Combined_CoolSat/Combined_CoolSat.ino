dff1465df15861cce12e75d8e7cb4c4d8c00e588
hadibus <chris.jenkins20102012@gmail.com>
2016-03-02 21:09:34 -0700

    Made all output to console also output to Serial3 which is the radio.  When SSHed in and both radios are connected, that output will appear in putty, (or whatever is used?)

--- Combined_CoolSat/Combined_CoolSat.ino
+++ Combined_CoolSat/Combined_CoolSat.ino
@@ -62,34 +62,43 @@ void setup()
   delay(5000); 
   
   Serial.begin(115200); // Arduino IDE Monitor
+  Serial3.begin(57600);
 
   
   
   //******INITIALIZE SD CARD*********
   
   Serial.print("Initializing SD card..."); // Initiallizes SD Card in Port 22.
+  Serial3.print("Initializing SD card...");
 
   if (!SD.begin(22)) 
   {
     Serial.println("initialization failed!"); //if initialization of SD Card fails, should stop here.
+    Serial3.println("initialization failed!");
     return;
   }
   Serial.println("initialization done."); //Done initializing the SD Card.
+  Serial3.println("initialization done.");
   Serial.println("");
+  Serial3.println("");
 
   
 
   //*******INITIALIZE TEMPERATURE SENSOR*********
 
   Serial.print("Initializing Temperature Sensor..."); //Initializing the Temperature Sensor
+  Serial3.print("Initializing Temperature Sensor...");
   
   if (!tempsensor.begin()) 
   {
     Serial.println("Couldn't find MCP9808!"); //if initilization fails, will stop here.
+    Serial3.println("Couldn't find MCP9808!");
     while (true);
   }
   Serial.println("Initialization done.");  // done initializing the Temperature Sensor
+  Serial3.println("Initialization done.");
   Serial.println("");
+  Serial3.println("");
 
   
   #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -103,14 +112,18 @@ void setup()
   //*******INITIALIZE ACCELEROMETER AND GYROSCOPE*********
 
   Serial.println("Initializing I2C devices...");
+  Serial3.println("Initializing I2C devices...");
   accelgyro.initialize();
 
   // verify connection
   Serial.println("Testing device connections...");
+  Serial3.println("Testing device connections...");
   Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+  Serial3.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
 
   Serial.println("Waiting for a command..."); // Waiting for a command from the user, can be used to take a picture, take the temperature, or speed.
+  Serial3.println("Waiting for a command...");
 }
 
 
@@ -166,29 +179,39 @@ void runCamera()
   digitalWrite(ledPinCam, HIGH); // Turning LED on to say the picture data is being received.
   camera.takePicture(); // Taking a picture.
   Serial.print("Image Size: ");
+  Serial3.print("Image Size: ");
   Serial.println(camera.imageSize, DEC);
+  Serial3.println(camera.imageSize, DEC);
   Serial.print("Number of Packages: ");
+  Serial3.print("Number of Packages: ");
   Serial.println(camera.numberOfPackages(), DEC); // printing us the data of the image size and how many packages there are.
+  Serial3.println(camera.numberOfPackages(), DEC);
 
   while(bytes = camera.getData())
   {
     // while the bytes are getting the data? loop through.
     for (short x = 0; x < bytes; x++)
     {
-      Serial.print("0x"); // printing out to the console
+      Serial.print("0x"); // printing out to the console 
       Serial.print(camera.imgBuffer[x], HEX);
       Serial.print(" ");
+      Serial3.print("0x");
+      Serial3.print(camera.imgBuffer[x], HEX);
+      Serial3.print(" ");
       myFile.print("0x"); // printing out to the text file.
       myFile.print(camera.imgBuffer[x], HEX);
       myFile.print(" ");
     }
   }
   Serial.println(""); 
+  Serial3.println("");
   myFile.println("");
   Serial.println("Done Downloading"); // telling us that we have successfully received the photo.
+  Serial3.println("Done Downloading");
   digitalWrite(ledPinCam, LOW); //turns LED off to tell us that the picture is done downloading.
   myFile.close(); //closing the text file.
   Serial.println("Waiting for a command..."); // asking for another user input.
+  Serial3.println("Waiting for a command...");
   pic[4] = pic[4] + 1;  // increments the file so we can have more than one text file.
     // TODO: add something here that will delete a text file if it's reached more than n (n will be what we decide) files.
 }
@@ -204,6 +227,7 @@ void runTemperatureSensor()
       
   myFile = SD.open(tempData.c_str(), FILE_WRITE); // opening a text file for reading Temperature Data
   //myFile = SD.open("Temp.txt", FILE_WRITE);
+  
       
   // Read and print out the temperature, then convert to *F
       
@@ -215,7 +239,9 @@ void runTemperatureSensor()
     float c = tempsensor.readTempC();
     float f = c * 9.0 / 5.0 + 32;  // conversion to Farenheight
     Serial.print("Temp: "); Serial.print(c); Serial.print("*C\t"); // Printing to the console
+    Serial3.print("Temp: "); Serial3.print(c); Serial3.print("*C\t"); // Printing to the console
     Serial.print(f); Serial.println("*F");
+    Serial3.print(f); Serial3.println("*F");
     myFile.print("Temp: "); myFile.print(c); myFile.print("*C\t");  // Printing to the text file.
     myFile.print(f); myFile.println("*F");
     delay(250);
@@ -224,13 +250,14 @@ void runTemperatureSensor()
   }
   myFile.close(); // Closing Temperature Text File
   Serial.println("Done taking Temperature.");
+  Serial3.println("Done taking Temperature.");
   Serial.println("Waiting for a command...");
+  Serial3.println("Waiting for a command...");
   tempsensor.shutdown_wake(1); // shutdown MSP9808 - power consumption ~0.1 mikro Ampere
  
   digitalWrite(ledPinTemp, LOW); // Turn the LED OFF to tell us it's done recording data.
   tempData[5] = tempData[5] + 1;  // increments the file so we can have more than one text file.
 
-  lightSensor = true;
 
   // TODO: add something here that will delete a text file if it's reached more than n (n will be what we decide) files.
 }
@@ -255,6 +282,10 @@ void runGyroSensor()
       Serial.print(ax); Serial.print("\t");
       Serial.print(ay); Serial.print("\t");
       Serial.print(az); Serial.println("");
+      Serial3.println("Acceleration X, Y, Z:\t");
+      Serial3.print(ax); Serial3.print("\t");
+      Serial3.print(ay); Serial3.print("\t");
+      Serial3.print(az); Serial3.println("");
       myAccel.println("Acceleration X, Y, Z: ");
       myAccel.print(ax); myAccel.print(",");
       myAccel.print(ay); myAccel.print(",");
@@ -263,6 +294,10 @@ void runGyroSensor()
       Serial.print(gx); Serial.print("\t");
       Serial.print(gy); Serial.print("\t");
       Serial.println(gz); Serial.println("");
+      Serial3.println("Gyro X, Y, Z:\t");
+      Serial3.print(gx); Serial3.print("\t");
+      Serial3.print(gy); Serial3.print("\t");
+      Serial3.println(gz); Serial3.println("");
       myGyro.println("Gyro X, Y, Z: ");
       myGyro.print(gx); myGyro.print(",");
       myGyro.print(gy); myGyro.print(",");
@@ -276,10 +311,18 @@ void runGyroSensor()
       Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
       Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
       Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
+
+      Serial3.write((uint8_t)(ax >> 8)); Serial3.write((uint8_t)(ax & 0xFF));
+      Serial3.write((uint8_t)(ay >> 8)); Serial3.write((uint8_t)(ay & 0xFF));
+      Serial3.write((uint8_t)(az >> 8)); Serial3.write((uint8_t)(az & 0xFF));
+      Serial3.write((uint8_t)(gx >> 8)); Serial3.write((uint8_t)(gx & 0xFF));
+      Serial3.write((uint8_t)(gy >> 8)); Serial3.write((uint8_t)(gy & 0xFF));
+      Serial3.write((uint8_t)(gz >> 8)); Serial3.write((uint8_t)(gz & 0xFF));
     #endif
     delay(2000);
   }
   Serial.println("Waiting for a command...");
+  Serial3.println("Waiting for a command...");
   digitalWrite(ledPinGy, LOW);
   myGyro.close();
   myAccel.close();
@@ -291,14 +334,18 @@ void runGyroSensor()
 //*****Wire Cutter Function*****
 void cutWire()
 {
+  Serial.println("Wire cutter");
+  Serial3.println("Wire cutter:");
   // turn Cutter on:
   digitalWrite(wireCutter, HIGH);
   Serial.println("On");
+  Serial3.println("On");
   delay(3000);
     
   // turn LED off:
   digitalWrite(wireCutter, LOW);
   Serial.println("Off");
+  Serial3.println("Off");
   delay(5000);
 }
 
@@ -316,12 +363,18 @@ void runLightSensor()
     lightReading = analogRead(lightSensorPin) * .0049;
 
     Serial.print(lightReading);
+    Serial3.print(lightReading);
     Serial.println(" Volts.");
+    Serial3.println(" Volts.");
     
-    if (lightReading > 1) 
+    if (lightReading > 1){
       Serial.println("The Sun is up.");
-    else 
+      Serial3.println("The Sun is up.");
+    }
+    else {
       Serial.println("The Sun is down.");
+      Serial3.println("The Sun is down.");
+    }
     
     delay(2000);
   }
